6171304e68ef578c42bc78a596005c4708d87059
Bartkk <TheBartkk@gmail.com>
2023-03-09 20:27:34 +0100

    show connetctions

--- scripts/ent/entity.gd
+++ scripts/ent/entity.gd
@@ -1,8 +1,8 @@
+class_name lumina_entity
 extends Node3D
 
-class_name lumina_entity
-
 func _process(delta):
+	draw_links()
 	if get_node_or_null("dev") != null:
 			get_node("dev").visible = util.developer
 
@@ -20,3 +20,9 @@ func error(msg):
 
 func msg(msg):
 	print("[" + str(self) + "] " + str(msg))
+
+func draw_links():
+	if "target" in self:
+			if self.target != null:
+				DebugDraw.draw_arrow_line(self.global_position, self.target.global_position,Color.DEEP_PINK,0.1,true)
+			
--- scripts/ent/logic/ent_propagator.gd
+++ scripts/ent/logic/ent_propagator.gd
@@ -1,6 +1,6 @@
 extends lumina_entity
 
-@export var targets : Array = []
+@export var targets : Array[lumina_entity] =  [lumina_entity.new()]
 
 func trigger():
 	msg("propagating triger signal")
@@ -9,10 +9,14 @@ func trigger():
 			util.notif(str(self) + " " + str(target) + " isn't a lumina_entity or doesn't exist.")
 			return
 
-		target = get_node_or_null(target)
-		
 		if target is lumina_entity:
 			target.trigger()
 		else:
 			util.notif(str(self) + " " + str(target) + " isn't a lumina_entity or doesn't exist.")
 			return
+
+func draw_links():
+	for target in targets:
+		if not target is lumina_entity:
+			continue
+		DebugDraw.draw_arrow_line(self.global_position, target.global_position,Color.CORNFLOWER_BLUE,0.1,true)
